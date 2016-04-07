25d1b612f05ef9911d5edc1bdaacb407138996ea
prsys <prsys@vagrant-centos65.vagrantup.com>
2016-04-07 21:45:54 +0000

    god修正

--- config/god/development.rb
+++ config/god/development.rb
@@ -6,10 +6,15 @@ rails_env = ENV['RAILS_ENV']
 root_dir = "/data/#{system_user}/#{app_name}"
 log_dir = "/data/#{system_user}/log"
 
+pid_dir = File.join(root_dir, 'tmp/pids')
+socket_dir = File.join(root_dir, 'tmp/sockets')
+FileUtils.mkdir_p(pid_dir) unless FileTest.exist?(pid_dir)
+FileUtils.mkdir_p(socket_dir) unless FileTest.exist?(socket_dir)
+
 God.watch do |w|
     w.name = 'unicorn'
     w.interval = 3.seconds
-    w.pid_file = File.join(root_dir, 'tmp/pids/unicorn.pid')
+    w.pid_file = File.join(pid_dir, 'unicorn.pid')
     w.env = { 'RAILS_ENV' => rails_env, 'BUNDLE_GEMFILE' => File.join(root_dir, 'Gemfile') }
 
     w.start = "cd #{root_dir} && bundle exec unicorn -c #{File.join(root_dir, "config/unicorn/#{rails_env}.rb")} -E #{rails_env} -D"
--- config/god/production.rb
+++ config/god/production.rb
@@ -3,16 +3,21 @@ app_name = 'webapp'
 
 rails_env = ENV['RAILS_ENV']
 
-root_dir = Pathname.new("/data/#{system_user}/#{app_name}")
-log_dir = Pathname.new("/data/#{system_user}/log")
+root_dir = "/data/#{system_user}/#{app_name}"
+log_dir = "/data/#{system_user}/log"
+
+pid_dir = File.join(root_dir, 'tmp/pids')
+socket_dir = File.join(root_dir, 'tmp/sockets')
+FileUtils.mkdir_p(pid_dir) unless FileTest.exist?(pid_dir)
+FileUtils.mkdir_p(socket_dir) unless FileTest.exist?(socket_dir)
 
 God.watch do |w|
     w.name = 'unicorn'
     w.interval = 3.seconds
-    w.pid_file = root_dir.join('tmp/pids/unicorn.pid')
-    w.env = { 'RAILS_ENV' => rails_env, 'BUNDLE_GEMFILE' => root_dir.join('Gemfile') }
+    w.pid_file = File.join(pid_dir, 'unicorn.pid')
+    w.env = { 'RAILS_ENV' => rails_env, 'BUNDLE_GEMFILE' => File.join(root_dir, 'Gemfile') }
 
-    w.start = "cd #{root_dir} && bundle exec unicorn -c #{root_dir.join("config/unicorn/#{rails_env}.rb")} -E #{rails_env} -D"
+    w.start = "cd #{root_dir} && bundle exec unicorn -c #{File.join(root_dir, "config/unicorn/#{rails_env}.rb")} -E #{rails_env} -D"
     w.stop = "kill -QUIT `cat #{w.pid_file}`"
     w.restart = "kill -USR2 `cat #{w.pid_file}`"
 
